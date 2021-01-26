990b05d46a4267591a241b89e98fd0e29fbd2c97
jameshwc <jameshwc@gmail.com>
2021-01-26 21:18:34 +0800

    add index.js as alternative & check whether username and password are valid

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-import { IgApiClient, SavedFeed, SavedFeedResponseLocation } from 'instagram-private-api';
-import { createWriteStream } from 'fs'
+const igapi = require('instagram-private-api');
+const IgApiClient = igapi.IgApiClient
+const fs = require('fs')
+const createWriteStream = fs.createWriteStream
 
 const username = process.env.IG_USERNAME
 const password = process.env.IG_PASSWORD
 
 if (username == "" || password == "" || username === undefined || password === undefined) {
-  console.log("username or password empty! you must provide them as env variable: $IG_USERNAME, $IG_PASSWORD")
-  process.exit(1)
+    console.log("username or password empty! you must provide them as env variable: $IG_USERNAME, $IG_PASSWORD")
+    process.exit(1)
 }
 
-async function fetchSavedPosts(feed: SavedFeed): Promise<SavedFeedResponseLocation[]> {
+async function fetchSavedPosts(feed) {
   const items = await feed.items()
-  const loc: SavedFeedResponseLocation[] = []
+  const loc = []
     for (let i = 0; i < items.length; i++) {
         if (items[i].location === undefined) {
             console.log(`https://www.instagram.com/p/${items[i].code}`)
@@ -33,7 +35,7 @@ ig.state.generateDevice(username);
   process.nextTick(async () => await ig.simulate.postLoginFlow());
     const userFeed = ig.feed.saved()
     // console.log(userFeed)
-  fetchSavedPosts(userFeed).then(loc => {
+    fetchSavedPosts(userFeed).then(loc => {
     var stream = createWriteStream("location.csv");
     stream.once('open', function(fd) {
       stream.write("Latitude,Longitude,Description,Address\n");
